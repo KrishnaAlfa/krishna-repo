0194f7a5376917c2beab2bea394ea19f6e52972c
KrishnaAlfa <64315971+KrishnaAlfa@users.noreply.github.com>
2020-04-25 20:28:58 +0530

    Update SecondAdd.java

--- src/main/java/com/git/git_demo/SecondAdd.java
+++ src/main/java/com/git/git_demo/SecondAdd.java
@@ -5,7 +5,7 @@ public class SecondAdd {
 	public static int addCheck(int a,int b) {
 		return a+b;
 	}
-	
+	// for pull request changes added here.
 	public static void main(String[] args) {
 System.out.println(SecondAdd.addCheck(4, 6));
 
