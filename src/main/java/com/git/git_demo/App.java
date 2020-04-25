ac0698e6a96e84d8ff4092edc446a4c379186f15
unknown <thakur.krishna09@gmail.com>
2020-04-25 19:19:05 +0530

    here

---
+++ src/main/java/com/git/git_demo/App.java
@@ -0,0 +1,19 @@
+package com.git.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	
+	public void getData(String name) {
+		System.out.println("Name is : "+name);
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
